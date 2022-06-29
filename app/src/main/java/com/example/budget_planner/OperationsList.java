18dd9a817847dd090faf4ae62fa42129291cc2ad
Free-Hugs <kkdo236@gmail.com>
2022-06-29 23:38:15 +0200

    Back button in OperationList fixed

--- app/src/main/java/com/example/budget_planner/OperationsList.java
+++ app/src/main/java/com/example/budget_planner/OperationsList.java
@@ -2,9 +2,12 @@ package com.example.budget_planner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class OperationsList extends AppCompatActivity {
@@ -19,5 +22,14 @@ public class OperationsList extends AppCompatActivity {
         ArrayAdapter<Double> operations = new ArrayAdapter<Double>(list.getContext(), R.layout.operation_list);
         db.latestOp(operations);
         list.setAdapter(operations);
+
+        Button returnButton = findViewById(R.id.opReturnButton);
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(OperationsList.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
